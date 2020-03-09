3df1dc9ab1a359b1160ca0b3dfd8d28044d4616f
Wesley Martin <wes@plusnarrative.com>
2020-03-09 10:45:08 +0200

    Added favicons

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield("title", '{"MTN":"Developer Portal"}')</title>
+    <link rel="icon" href="/images/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="/images/favicon.png" type="image/png">
     <link rel="stylesheet" href="{{ mix('/css/styles.css') }}">
     @stack("styles")
     <link rel="preload" href="/fonts/MTNBrighterSans-Regular.woff2" as="font" type="font/woff2">
