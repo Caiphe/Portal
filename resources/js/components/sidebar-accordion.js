f2e62c01331725ae99df44c1e0b7e5ab56ec08d3
Xoliswa <xoliswa@plusnarrative.com>
2020-03-19 23:05:05 +0200

    Sidebar component ready for review

--- resources/js/components/sidebar-accordion.js
+++ resources/js/components/sidebar-accordion.js
@@ -7,22 +7,35 @@
             submenu = el.querySelector('.accordion-sub-items'),
             arrow = button.querySelector('i.icon-arrow');
         
-            button.onclick = function (event) {
-                if (!submenu.hasClass('show')) {
-                    submenu.classList.add('show');
-                    submenu.classList.remove('hide');
-                    arrow.classList.add('open');
-                    arrow.classList.remove('close');
-                    event.preventDefault();
-                }
-                else {
-                    submenu.classList.remove('show');
-                    submenu.classList.add('hide');
-                    arrow.classList.remove('open');
-                    arrow.classList.add('close');
-                    event.preventDefault();
-                }
-            };
+        //accordion open and close items
+        button.onclick = function (event) {
+            if (!submenu.hasClass('show')) {
+                submenu.classList.add('show');
+                submenu.classList.remove('hide');
+                arrow.classList.add('open');
+                arrow.classList.remove('close');
+                event.preventDefault();
+            }
+            else {
+                submenu.classList.remove('show');
+                submenu.classList.add('hide');
+                arrow.classList.remove('open');
+                arrow.classList.add('close');
+                event.preventDefault();
+            }
+        };
+        
+        //add active class to sub-item onclick
+        var menu = document.querySelector('.accordion-menu');
+        submenu.onclick = function (event) {            
+            var submitemActive = menu.querySelectorAll('.accordion-sub-items li a.active');
+            if (submitemActive.length > 0) {
+                submitemActive.forEach(function (item) {
+                    item.classList.remove('active');
+                });
+            }
+            event.target.classList.add('active');
+        }
     })
     
     Element.prototype.hasClass = function (className) {
--- resources/views/components/sidebar-accordion.blade.php
+++ resources/views/components/sidebar-accordion.blade.php
@@ -1,25 +1,24 @@
-@props(['list'])
+@props(['id', 'list'])
 @allowonce('sidebar_accordion')
 <link href="/css/components/sidebar-accordion.css" rel="stylesheet"/>
 @endallowonce
 
 <div id="sidebar-accordion" {{ $attributes->merge(['class' => 'accordion-items']) }}>
-    @foreach ($list as $accordion => $items) 
-
-        <ul class="accordion-menu">
-            <li class="accordion-item"><a href="#" data-toggle="accordion-item"> {{ $accordion }} <i class="icon-arrow"></i></a>
+    <ul id="{{ $id }}" class="accordion-menu">    
+        @foreach ($list as $accordion => $items) 
+            <li class="accordion-item"><a href="#" data-toggle="accordion-item"> {{ $accordion }} <i class="icon-arrow">@svg('chevron-right', '#000000')</i></a>
                 <ul class="accordion-sub-items">
                 @if(is_array($items)) 
                     @foreach($items as $sub_item) 
-                        <li class="accordion-sub-item"><a href="{{ $sub_item['link'] }}">{{$sub_item['label']}}</a></li>
+                        <li class="accordion-sub-item"><a href="{{ $sub_item['link'] }}">{{$sub_item['label']}}<i class="arrow-forward">@svg('arrow-forward', '#000000')</i></a></li>
                     @endforeach
                 @else  
-                    <li class="accordion-sub-item"><a href="#">{{ $items }} </a></li>
+                    <li class="accordion-sub-item"><a href="#">{{ $items }} <i class="arrow-forward">@svg('arrow-forward', '#000000')</i></a></li>
                 @endif 
                 </ul>
             </li>
-        </ul>
-    @endforeach
+        @endforeach
+    </ul>
 </div>
 
 @pushscript('sidebar_accordion')
