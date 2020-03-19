75eb05f222a64438207417372ea75938b88d1492
Waseem Dawood <waseem@plusnarrative.com>
2020-03-19 11:46:21 +0200

    ran npm run prod, added comments

--- resources/views/components/card-product.blade.php
+++ resources/views/components/card-product.blade.php
@@ -1,7 +1,16 @@
+{{-- 
+    This component allows for adding product cards.
+    Eg:
+    <x-card-product :countries="$countries" :tags="$tags" target="_blank" title="API Name" href="http://www.google.com">This is the description</x-card-product>
+    countries - is an array of country names, which is converted to slug friendly text for the country svg files e.g. ['South Africa','Cameroon','Ivory Coast']
+	tags -  is an array the product tags e.g. ['MTN API','MTN APP']
+	title - the card title
+	The card description is passed through the $slot and link attributes can be added to the card to be applied to the a tag
+--}}
+
 @allowonce('card_product')
 <link href="/css/components/card-product.css" rel="stylesheet"/>
 <link href="/css/components/card.css" rel="stylesheet"/>
-<!-- <script src="/js/components/card-product.js"></script> -->
 @endallowonce
 
 @props(['title','countries','tags'])
