2dc6a7a1573ba0e0afda06df6658075b1ec0cf90
Wesley Martin <wes@plusnarrative.com>
2020-03-24 15:52:30 +0200

    Added fab to header

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <x-heading :heading="$product->display_name" tags="hello, wolrd"/>
+    <x-heading :heading="$product->display_name" fab="dark plus"/>
 
     <div>this is some content</div>
 @endsection
