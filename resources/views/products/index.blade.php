b3ce19c9a986cccffd9842414b18b23a1bb72c96
Waseem Dawood <waseem@plusnarrative.com>
2020-03-24 16:06:59 +0200

    merge develop

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,13 +4,20 @@
 <link rel="stylesheet" href="/css/templates/products/index.css">
 @endpush
 
-@section('title', 'Products')
+
+<x-heading heading="Products"></x-heading>
 
 @section('content')
     @foreach ($products_collection as $category=>$products)
 		<h3>{{ $category }}</h3>
 		@foreach ($products as $product)
-			<x-card-product :title="$product->display_name" class="product-block" :href="$product->slug" >{{ $product->description }}</x-card-product></div>
+			@php //setting variables
+			if ($product->locations !== 'all' || $product->locations !== NULL) :
+				$countries = explode(',',$product->locations);
+			endif;
+			$tags = array($product->group,$product->category);
+			@endphp
+			<x-card-product :title="$product->display_name" class="product-block" :href="$product->slug" :countries="$countries" :tags="$tags">{{ $product->description }}</x-card-product></div>
 		@endforeach	
 	@endforeach
 @endsection
