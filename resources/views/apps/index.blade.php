c8b240e691da822eadab3d1fe1b9ce2aacb0e21a
Jethro May <jethro@plusnarrative.com>
2020-03-25 16:30:54 +0200

    Migrated markup for app component into component file

--- resources/views/apps/index.blade.php
+++ resources/views/apps/index.blade.php
@@ -17,7 +17,7 @@
     <div class="container" id="app-index">
         <div class="row">
             <div>
-                <div style="display: flex; align-items: center;">
+                <div class="approved-apps">
                     @svg('chevron-down', '#000000')
 
                     <h3 style="margin-left: 10px">Approved Apps</h3>
@@ -51,66 +51,7 @@
                     </div>
                     <div class="body">
                         @foreach($approved_apps as $app)
-                            <div class="my-app">
-                                <div class="column">
-                                    <p class="name">
-                                        @svg('app-avatar', '#fff')
-                                        {{ $app['name'] }}
-                                    </p>
-                                </div>
-                                <div class="column regions">
-                                    @svg('za', '#000000', 'images/locations')
-                                    @svg('af', '#000000', 'images/locations')
-                                    @svg('bf', '#000000', 'images/locations')
-                                </div>
-                                <div class="column">
-                                    <a href="">https://www.appdomain.co.za</a>
-                                </div>
-                                <div class="column">
-                                    {{ \Carbon\Carbon::parse()->format('d M') }}
-                                </div>
-                                <div class="column">
-                                    <svg height="25" width="25">
-                                        <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
-                                    </svg>
-                                </div>
-                                <div class="column">
-                                    <span class="actions"></span>
-{{--                                    <nav class="menu">--}}
-{{--                                        <a>Analytics</a>--}}
-{{--                                        <a>Edit</a>--}}
-{{--                                        <a>Delete</a>--}}
-{{--                                    </nav>--}}
-                                </div>
-                                <div class="detail">
-                                    <p>Consumer key: M5dGYCRA4FEtfccmBH6IGFp8RRddMivK</p>
-                                    <p>Consumer secret: 6DxjvPOWlpzTbGe7</p>
-                                    <p>Key issued: 02/03/2020 - 21:26</p>
-                                    <p>Expires: Never</p>
-                                    <p>Callback URL: https://www.plusnarrative.com</p>
-                                    <p>Description: Test application</p>
-
-                                    <p>Products</p>
-
-                                    <div class="products">
-                                        <div class="product">
-                                            <svg height="25" width="25">
-                                                <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
-                                            </svg>
-                                            <span>Subscription v1</span>
-                                            @svg('arrow-forward', '#000000')
-                                        </div>
-
-                                        <div class="product">
-                                            <svg height="25" width="25">
-                                                <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
-                                            </svg>
-                                            <span>SMS</span>
-                                            @svg('arrow-forward', '#000000')
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <x-app :name="$app['name']"></x-app>
                         @endforeach
                     </div>
                 </div>
@@ -119,7 +60,7 @@
 
         <div class="row" id="app">
             <div>
-                <div style="display: flex; align-items: center;">
+                <div class="revoked-apps">
                     @svg('chevron-down', '#000000')
 
                     <h3 style="margin-left: 10px">Revoked Apps</h3>
@@ -153,64 +94,7 @@
                     </div>
                     <div class="body">
                         @foreach($revoked_apps as $app)
-                            <div class="my-app">
-                                <div class="column">
-                                    <p class="name">
-                                        @svg('app-avatar', '#fff')
-                                        {{ $app['name'] }}
-                                    </p>
-                                </div>
-                                <div class="column">
-                                    Lorem ipsum dolor sit amet, consetetur.
-                                </div>
-                                <div class="column">
-                                    <a href="">https://www.appdomain.co.za</a>
-                                </div>
-                                <div class="column">
-                                    {{ \Carbon\Carbon::parse()->format('d M') }}
-                                </div>
-                                <div class="column">
-                                    <svg height="25" width="25">
-                                        <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
-                                    </svg>
-                                </div>
-                                <div class="column">
-                                    <span class="actions"></span>
-{{--                                    <nav class="menu">--}}
-{{--                                        <a>Analytics</a>--}}
-{{--                                        <a>Edit</a>--}}
-{{--                                        <a>Delete</a>--}}
-{{--                                    </nav>--}}
-                                </div>
-                                <div class="detail">
-                                    <p>Consumer key: M5dGYCRA4FEtfccmBH6IGFp8RRddMivK</p>
-                                    <p>Consumer secret: 6DxjvPOWlpzTbGe7</p>
-                                    <p>Key issued: 02/03/2020 - 21:26</p>
-                                    <p>Expires: Never</p>
-                                    <p>Callback URL: https://www.plusnarrative.com</p>
-                                    <p>Description: Test application</p>
-
-                                    <p>Products</p>
-
-                                    <div class="products">
-                                        <div class="product">
-                                            <svg height="25" width="25">
-                                                <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
-                                            </svg>
-                                            <span>Subscription v1</span>
-                                            @svg('arrow-forward', '#000000')
-                                        </div>
-
-                                        <div class="product">
-                                            <svg height="25" width="25">
-                                                <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
-                                            </svg>
-                                            <span>SMS</span>
-                                            @svg('arrow-forward', '#000000')
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <x-app :name="$app['name']"></x-app>
                         @endforeach
                     </div>
                 </div>
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,20 +1,63 @@
 @props(['name'])
 
-<tr {{ $attributes }}>
-    {{ dd($app) }}
-    <th>{{ $name }}</th>
-    <th>Lorem ipsum dolor sit amet, consetetur.</th>
-    <th>
+<div class="my-app" {{ $attributes }}>
+    <div class="column">
+        <p class="name">
+            @svg('app-avatar', '#fff')
+            {{ $name }}
+        </p>
+    </div>
+    <div class="column regions">
+        @svg('za', '#000000', 'images/locations')
+        @svg('af', '#000000', 'images/locations')
+        @svg('bf', '#000000', 'images/locations')
+    </div>
+    <div class="column">
         <a href="">https://www.appdomain.co.za</a>
-    </th>
-    <th></th>
-    <th>
+    </div>
+    <div class="column">
+        {{ \Carbon\Carbon::parse()->format('d M') }}
+    </div>
+    <div class="column">
         <svg height="25" width="25">
             <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
         </svg>
-    </th>
-    <th>
+    </div>
+    <div class="column">
         <span class="actions"></span>
-    </th>
-</tr>
+        {{--                                    <nav class="menu">--}}
+        {{--                                        <a>Analytics</a>--}}
+        {{--                                        <a>Edit</a>--}}
+        {{--                                        <a>Delete</a>--}}
+        {{--                                    </nav>--}}
+    </div>
+    <div class="detail">
+        <p>Consumer key: M5dGYCRA4FEtfccmBH6IGFp8RRddMivK</p>
+        <p>Consumer secret: 6DxjvPOWlpzTbGe7</p>
+        <p>Key issued: 02/03/2020 - 21:26</p>
+        <p>Expires: Never</p>
+        <p>Callback URL: https://www.plusnarrative.com</p>
+        <p>Description: Test application</p>
+
+        <p>Products</p>
+
+        <div class="products">
+            <div class="product">
+                <svg height="25" width="25">
+                    <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
+                </svg>
+                <span>Subscription v1</span>
+                @svg('arrow-forward', '#000000')
+            </div>
+
+            <div class="product">
+                <svg height="25" width="25">
+                    <circle cx="12.5" cy="12.5" r="10" stroke="#BB1E4F" stroke-width="3" fill="#BB1E4F" />
+                </svg>
+                <span>SMS</span>
+                @svg('arrow-forward', '#000000')
+            </div>
+        </div>
+    </div>
+</div>
 
