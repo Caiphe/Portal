63abde4ec3f6126f706ae1fa82b53f00768488a6
Wesley Martin <wes@plusnarrative.com>
2020-03-26 16:27:14 +0200

    Added flex and flex-wrap to tags

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,6 @@
-function toggleProfileMenu(){document.body.classList.toggle("show-profile-menu")}document.getElementById("profile").addEventListener("click",toggleProfileMenu),document.getElementById("profile-menu-background").addEventListener("click",toggleProfileMenu);
+document.getElementById('profile').addEventListener('click', toggleProfileMenu);
+document.getElementById('profile-menu-background').addEventListener('click', toggleProfileMenu);
+
+function toggleProfileMenu() {
+    document.body.classList.toggle('show-profile-menu');
+}
