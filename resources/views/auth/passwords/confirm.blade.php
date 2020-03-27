112a1d18fea0f7cacf067c64e7aa11d591675096
Tebello Ramantso <tebello@plusnarrative.com>
2020-03-27 07:59:36 +0200

    Added view elements for the Sign Up confirm page.

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,65 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+<div class="row m-0">
+    <div style="padding: 50px; width: 35vw; height: 100vh; float: left;">
+        <div class="row m-0 step__wizzard_header">
+            <img style="width: 60px;"src="/images/mtn-logo.svg" alt="MTN logo">
+            <h2 style="padding-top: 5px; margin-left: 30px;">Developer Portal</h2>
+        </div>
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+        <div >
+            <h2>Almost there...</h2>
+            <p>
+                A confirmation email has been sent to your email address. 
+                Please click on the link in the email to verify your email address. <br/><br/>
+                If you are not receiving the cnfirmation email, please read out FAQ article here.
+            </p>
 
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <button style="display: inline;">Resend verification</button>
         </div>
     </div>
+    <!-- <div class="col-md-8 m-0 p-0" style="background-image: url('/images/carousel-placeholder-img.png'); background-position: center; background-size: contain; padding: 10px; min-height: 100vh;"></div> -->
+    <x-carousel style="width: 65vw; height: 100vh; float: left;" wait="5000" duration="0.34">
+        <x-carousel-item style="background-image: url('/images/mtn-carousel-img-01.png'); background-position: center; background-size: cover; background-repeat: no-repeat;">
+        <div style="position: absolute; left: 20%; margin-left: -50px; bottom: 14%; margin-bottom: -50px; max-width: 800px;">
+            <h1 style="color: #fff; font-size: 6em;">Create an account</h1>
+            <p style="color: #fff; display: block; font-size: 1.5em; line-height: 1.6em;">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
+            </p>
+        </div>
+        </x-carousel-item>
+
+        <x-carousel-item style="background-image: url('/images/mtn-carousel-img-02.png'); background-position: center; background-size: cover; background-repeat: no-repeat;">
+            <div style="position: absolute; left: 20%; margin-left: -50px; bottom: 14%; margin-bottom: -50px; max-width: 800px;">
+                <h2 style="color: #fff; font-size: 6em;">Register today!</h2>
+                <p style="color: #fff; display: block; font-size: 1.5em; line-height: 1.6em;">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
+                </p>
+            </div>
+        </x-carousel-item>
+
+        <x-carousel-item style="background-image: url('/images/mtn-carousel-img-01.png'); background-position: center; background-size: cover; background-repeat: no-repeat;">
+            <div style="position: absolute; left: 20%; margin-left: -50px; bottom: 14%; margin-bottom: -50px; max-width: 800px;">
+                <h2 style="color: #fff; font-size: 6em;">Create an account</h2>
+                <p style="color: #fff; display: block; font-size: 1.5em; line-height: 1.6em;">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
+                </p>
+            </div>
+        </x-carousel-item>
+
+        <x-carousel-item style="background-image: url('/images/mtn-carousel-img-02.png'); background-position: center; background-size: cover; background-repeat: no-repeat;">
+            <div style="position: absolute; left: 20%; margin-left: -50px; bottom: 14%; margin-bottom: -50px; max-width: 800px;">
+                <h2 style="color: #fff; font-size: 6em;">Join Us today</h2>
+                <p style="color: #fff; display: block; font-size: 1.5em; line-height: 1.6em;">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
+                </p>
+            </div>
+        </x-carousel-item>
+    </x-carousel>
 </div>
 @endsection
