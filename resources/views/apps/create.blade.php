cbab42f6c5235795c679fe6855ab0485756a14f3
Jethro May <jethro@plusnarrative.com>
2020-03-27 12:33:52 +0200

    Added markup

--- resources/views/apps/create.blade.php
+++ resources/views/apps/create.blade.php
@@ -41,28 +41,43 @@
         </nav>
 
         <div class="row">
-            @svg('app-avatar', '#ffffff')
-            <form class="active" action="">
 
-                <div class="group">
-                    <label for="name">Name your app *</label>
-                    <input type="text" name="name" id="name" placeholder="Enter name">
+            <form action="">
+{{--                @svg('app-avatar', '#ffffff')--}}
+{{--                <div>--}}
+{{--                    <div class="group">--}}
+{{--                        <label for="name">Name your app *</label>--}}
+{{--                        <input type="text" name="name" id="name" placeholder="Enter name">--}}
+{{--                    </div>--}}
+
+{{--                    <div class="group">--}}
+{{--                        <label for="name">Callback url *</label>--}}
+{{--                        <input type="text" name="name" id="name" placeholder="Enter callback url">--}}
+{{--                    </div>--}}
+
+{{--                    <div class="group">--}}
+{{--                        <label for="description">Description *</label>--}}
+{{--                        <textarea name="description" id="description" rows="5" placeholder="Enter description"></textarea>--}}
+{{--                    </div>--}}
+
+{{--                    <button class="dark" type="submit">--}}
+{{--                        Select regions--}}
+{{--                        @svg('arrow-forward', '#ffffff')--}}
+{{--                    </button>--}}
+{{--                </div>--}}
+
+                <div>
+                    <p style="font-size: 16px;">Select the regions you would like to associate with your app *</p>
+
+                    <div class="buttons">
+                        <button class="dark outline">Back</button>
+                        <button class="dark">
+                            Add products
+                            @svg('arrow-forward', '#ffffff')
+                        </button>
+                    </div>
                 </div>
 
-                <div class="group">
-                    <label for="name">Callback url *</label>
-                    <input type="text" name="name" id="name" placeholder="Enter callback url">
-                </div>
-
-                <div class="group">
-                    <label for="description">Description *</label>
-                    <textarea name="description" id="description" rows="5" placeholder="Enter description"></textarea>
-                </div>
-
-                <button class="dark" type="submit">
-                    Select regions
-                    @svg('arrow-forward', '#ffffff')
-                </button>
             </form>
         </div>
 
@@ -73,6 +88,6 @@
 
 @push('scripts')
 <script>
-
+    var state = document.querySelector('form .active');
 </script>
 @endpush
