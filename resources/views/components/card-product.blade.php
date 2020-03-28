77a4c8fd27c3883f912a2c5be09c664cb22fc6e9
Waseem Dawood <waseem@plusnarrative.com>
2020-03-28 13:42:03 +0200

    product card styling updates

--- resources/views/components/card-product.blade.php
+++ resources/views/components/card-product.blade.php
@@ -1,12 +1,14 @@
 {{-- 
     This component allows for adding product cards.
     Eg:
-    <x-card-product :countries="$countries" :tags="$tags" target="_blank" title="API Name" href="http://www.google.com">This is the description</x-card-product>
-    countries - is an array of country names, which is converted to slug friendly text for the country svg files e.g. ['South Africa','Cameroon','Ivory Coast']
-	tags -  is an array the product tags e.g. ['MTN API','MTN APP']
-	title - the card title
-	href - the link for the card to go to
-	The card description is passed through the $slot and link attributes can be added to the card to be applied to the a tag
+    <x-card-product :countries="$countries" :tags="$tags" target="_blank" title="API Name" href="http://www.google.com" addButtonId="button-id" addUrl="http://www.google.com">This is the description</x-card-product>
+    countries - is an array of country names, which is converted to slug friendly text for the country svg files e.g. ['za','cm','ci']
+    tags -  is an array the product tags e.g. ['MTN API','MTN APP']
+    title - the card title
+    href - the link for the card to go to
+    addButtonId - id for add button, if empty will hide the add button
+    addUrl - The url for the add button
+    The card description is passed through the $slot and link attributes can be added to the card to be applied to the a tag
 --}}
 
 @allowonce('card_product')
@@ -14,39 +16,41 @@
 <link href="/css/components/card.css" rel="stylesheet"/>
 @endallowonce
 
-@props(['title','countries','tags', 'href'])
-
+@props(['title','countries','tags', 'href', 'addButtonId', 'addUrl'])
 <div {{ $attributes->merge(['class' => 'card card--product']) }} >
-	<a href="{{$href}}">
-		<div class="card__content">
-			@isset($tags)
-				@foreach ($tags as $tag)
-					<span class="tag outline yellow">{{$tag}}</span>
-				@endforeach
-			@endisset
-			@isset($title)
-			<h3 class="card__header">
-				{{ $title }}
-			</h3>
-			@endisset
-			<p class="card__body">
-				{{ $slot }}
-			</p>
-			@isset($countries)
-			<div class="country-selector">
-				<div class="countries">
-					@foreach ($countries as $country)
-						<img src="/images/locations/{{$country}}.svg" title="{{$country}} flag" alt="{{$country}} flag">
-					@endforeach
-				</div>
-				@if (count($countries ) > 1)
-					<div class="view-more">+ {{count($countries )-1}} more</div>
-				@endif	
-			</div>	
-			@endisset	
-		</div>
-	</a>
-	<div class="buttons">
-		<button class="inline fab plus dark"></button>
-	</div>
+    <a href="{{$href}}">
+        <div class="card__content">
+            @isset($tags)
+                @foreach ($tags as $tag)
+                    <span class="tag outline yellow">{{$tag}}</span>
+                @endforeach
+            @endisset
+            @isset($title)
+            <h3 class="card__header">
+                {{ $title }}
+            </h3>
+            @endisset
+            <p class="card__body">
+                {{ $slot }}
+            </p>
+            @isset($countries)
+            <div class="country-selector">
+                <div class="countries">
+                    @foreach ($countries as $country)
+                        <img src="/images/locations/{{$country}}.svg" title="{{$country}} flag" alt="{{$country}} flag">
+                    @endforeach
+                </div>
+                @if (count($countries ) > 1)
+                    <div class="view-more">+ {{count($countries )-1}} more</div>
+                @endif  
+            </div>  
+            @endisset   
+        </div>
+    </a>
+    <div class="buttons">
+        <a class="inline button" href="{{$href}}">View</a>
+        @isset($addButtonId)
+        <a href="{{$addUrl}}" id="$addButtonId" class="inline button fab plus dark"></a>
+        @endisset
+    </div>
 </div>
