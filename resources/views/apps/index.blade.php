360368f2bd408454bee3e6f3ac80b4fd04fbeae6
Jethro May <jethro@plusnarrative.com>
2020-03-28 12:05:40 +0200

    Added delete prompt to app dashboard

--- resources/views/apps/index.blade.php
+++ resources/views/apps/index.blade.php
@@ -148,8 +148,8 @@
 
     var buttons = document.querySelectorAll('.name');
 
-    for (var i = 0; i < buttons.length; i ++) {
-        buttons[i].addEventListener('click', handleButtonClick);
+    for (var j = 0; j < buttons.length; j ++) {
+        buttons[j].addEventListener('click', handleButtonClick);
     }
 
     function handleButtonClick() {
@@ -164,8 +164,8 @@
 
     var actions = document.querySelectorAll('.actions');
 
-    for (var i = 0; i < actions.length; i ++) {
-        actions[i].addEventListener('click', handleMenuClick);
+    for (var k = 0; k < actions.length; k ++) {
+        actions[k].addEventListener('click', handleMenuClick);
     }
 
     function handleMenuClick() {
@@ -175,6 +175,29 @@
         parent.querySelector('.modal').classList.toggle('show');
     }
 
+    var modals = document.querySelectorAll('.modal');
+    for (var l = 0; l < modals.length; l ++) {
+        modals[l].addEventListener('click', function() {
+            document.querySelector(".modal.show").classList.remove('show');
+            document.querySelector(".menu.show").classList.remove('show');
+        })
+    }
+
+    var deleteButtons = document.querySelectorAll('.app-delete');
+    for (var m = 0; m < modals.length; m ++) {
+        deleteButtons[m].addEventListener('click', handleDeleteMenuClick);
+    }
+    function handleDeleteMenuClick(event) {
+        var app = event.currentTarget;
+
+        var deleteApp = confirm('Are you sure you want to delete this app?');
+
+        if(deleteApp) {
+            console.log('Delete');
+        }
+    }
+
+    // FIXME: COPYING KEY AND SECRET IS NOT WORKING.
     var keys = document.querySelectorAll('.copy');
 
     for (var i = 0; i < keys.length; i ++) {
@@ -187,14 +210,6 @@
         /* Copy the text inside the text field */
         document.execCommand("copy");
         el.blur();
-    }
-
-    var modals = document.querySelectorAll('.modal');
-    for (var i = 0; i < modals.length; i ++) {
-        modals[i].addEventListener('click', function() {
-            document.querySelector(".modal.show").classList.remove('show');
-            document.querySelector(".menu.show").classList.remove('show');
-        })
     }
 </script>
 @endpush
