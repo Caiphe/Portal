ed2e5c09ea5360934b4d691f6963dbf0586a8c49
Wesley Martin <wes@plusnarrative.com>
2020-03-30 13:32:30 +0200

    Adding the product dropdown. Fixing the profile dropdown

--- resources/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('profile').addEventListener('click', toggleProfileMenu);
-document.getElementById('profile-menu-background').addEventListener('click', toggleProfileMenu);
-
-function toggleProfileMenu() {
-    document.body.classList.toggle('show-profile-menu');
-}
