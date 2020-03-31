97bd75b9c7f836c87deddb1594f4357b0094e17f
Jethro May <jethro@plusnarrative.com>
2020-03-31 16:46:21 +0200

    Added further end points for app related actions

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::put('apps/{name}', 'AppController@update')->name('app.update');
 Route::delete('apps/{name}', 'AppController@destroy')->name('app.destroy');
 Route::post('apps/{product}/approve', 'DashboardController@approve')->name('app.product.approve');
 Route::post('apps/{product}/revoke', 'DashboardController@revoke')->name('app.product.revoke');
+Route::post('apps/{name}/products/approve', 'DashboardController@approveAll')->name('app.products.approve');
+Route::post('apps/{name}/products/revoke', 'DashboardController@revokeAll')->name('app.products.revoke');
+Route::post('apps/{name}/products/complete', 'DashboardController@complete')->name('app.products.complete');
 
 Route::get('products', 'ProductController@index');
 Route::get('products/{product:slug}', 'ProductController@show')->name('product.show');
