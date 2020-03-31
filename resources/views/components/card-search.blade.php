369cd90946f2cb154252238392713609dc427f7d
Xoliswa <xoliswa@plusnarrative.com>
2020-03-31 15:00:35 +0200

    card-search component

---
+++ resources/views/components/card-search.blade.php
@@ -0,0 +1,35 @@
+{{-- 
+    This component allows for adding search cards.
+    Eg:
+    <x-card-search  title="My Apps" href="http://www.google.com" link="http://www.google.com">This is the description</x-card-search>
+	title - the card title
+	The card description is passed through the $slot and search attributes can be added to the card to be applied to the a tag
+--}}
+
+@allowonce('card_search')
+<search href="/css/components/card-search.css" rel="stylesheet"/>
+<search href="/css/components/card.css" rel="stylesheet"/>
+@endallowonce
+
+@props(['title', 'icon', 'link'])
+
+@php 
+    $card_text = strlen($slot) > 165 ? substr($slot,0,165) : $slot;
+@endphp
+
+<a {{ $attributes }}>
+	<div class="card card--search">
+		<div class="card__content">
+			@isset($title)
+			<h3 class="card__header">
+				{{ $title }}
+			</h3>
+			@endisset
+			<p class="card__body">
+				{{ $card_text }}
+            </p>
+            <a href="{{ $link }}">{{ $link }}</a>
+            @svg('arrow-forward', '#000000')
+		</div>
+	</div>
+</a>	
