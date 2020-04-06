b5f7a2cfd333bc1d2f7d51e4ccb08e1e9f24a54f
Jethro May <jethro@plusnarrative.com>
2020-04-06 11:36:59 +0200

    Cleaning up styles

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,40 +1,44 @@
 <header id="header">
     <div class="header-inner container">
-        <a href="/"><img src="/images/mtn-logo.svg" alt="MTN logo"></a>
-        <a href="/" class="site-name">Developer Portal</a>
-        <ul class="main-menu" role="navigation" aria-label="Main">
-            <li class="has-children">
-                <a href="/products">Products @svg("chevron-down")</a>
-                <div class="product-nav shadow">
-                    <div class="nav-left">
-                        <a href="/products">Browse all our products</a>
-                        <a href="/getting-started">Working with our products</a>
+{{--        <nav>--}}
+            <a href="/"><img src="/images/mtn-logo.svg" alt="MTN logo"></a>
+            <a href="/" class="site-name">Developer Portal</a>
+            <ul class="main-menu" role="navigation" aria-label="Main">
+                <li class="has-children">
+                    <a href="/products">Products @svg("chevron-down")</a>
+                    <div class="product-nav shadow">
+                        <div class="nav-left">
+                            <a href="/products">Browse all our products</a>
+                            <a href="/getting-started">Working with our products</a>
+                        </div>
+                        <div class="nav-right">
+                            <h3>BROWSE BY CATEGORY</h3>
+                            @foreach($productCategories as $productCategory)
+                                <a href="/products/?category={{$productCategory}}">{{$productCategory}}</a>
+                            @endforeach
+                        </div>
                     </div>
-                    <div class="nav-right">
-                        <h3>BROWSE BY CATEGORY</h3>
-                        @foreach($productCategories as $productCategory)
-                        <a href="/products/?category={{$productCategory}}">{{$productCategory}}</a>
-                        @endforeach
-                    </div>
-                </div>
-            </li>
-			<li><a href="/faq">FAQ</a></li>
-			<li><a href="/contact">Contact us</a></li>
-        </ul>
-        <form action="{{route('search')}}"><input type="search" name="q" id="search" class="thin see-through" placeholder="Search"></form>
-        @if(\Auth::check())
-        <a href="/apps/create" class="button dark">Build app</a>
-        <div id="profile-menu">
-            <div id="profile-menu-picture" style="background-image: url({{\Auth::user()->profile_picture}})"></div>
-            <ul class="profile-menu-options shadow">
-                <li><a href="/profile">Profile</a></li>
-                <li><a href="/apps">Apps</a></li>
-                <li><form action="{{route('logout')}}" method="post">@csrf<button>Sign out</button></form></li>
+                </li>
+                <li><a href="/faq">FAQ</a></li>
+                <li><a href="/contact">Contact us</a></li>
             </ul>
-        </div>
-        @else
-        <a href="{{route('login')}}" class="button dark outline mr-1">Login</a>
-        <a href="{{route('register')}}" class="button dark">Register</a>
-        @endif
+            <form action="{{route('search')}}">
+                <input type="search" name="q" id="search" class="thin see-through" placeholder="Search">
+            </form>
+            @if(\Auth::check())
+                <a href="/apps/create" class="button dark" role="button">Build app</a>
+                <div id="profile-menu">
+                    <div id="profile-menu-picture" style="background-image: url({{\Auth::user()->profile_picture}})"></div>
+                    <ul class="profile-menu-options shadow">
+                        <li><a href="/profile">Profile</a></li>
+                        <li><a href="/apps">Apps</a></li>
+                        <li><form action="{{route('logout')}}" method="post">@csrf<button>Sign out</button></form></li>
+                    </ul>
+                </div>
+            @else
+                <a href="{{route('login')}}" class="button dark outline mr-1">Login</a>
+                <a href="{{route('register')}}" class="button dark">Register</a>
+            @endif
+{{--        </nav>--}}
     </div>
 </header>
