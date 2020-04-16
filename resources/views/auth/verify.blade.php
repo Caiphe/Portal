9b47dacc7876c3a8389094975c066d46d8ec4fff
Wesley Martin <wes@plusnarrative.com>
2020-04-16 11:58:45 +0200

    Updated typo

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -12,7 +12,7 @@
     @csrf
 	<h1>Almost there...</h1>
 	<p>A confirmation email has been sent to your email address. Please click on the link in the email to verify your email address.</p>
-	<p>If you are not receiving the cnfirmation email, please read out FAQ article <a href="/faq">here.</a></p>
+	<p>If you are not receiving the confirmation email, please read out FAQ article <a href="/faq">here.</a></p>
     <button type="submit">Resend verification</button>
 </form>
 <x-auth.carousel />
