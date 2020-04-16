27faf053cafea2265f32ee3ad4617a0bb25f67db
Wesley Martin <wes@plusnarrative.com>
2020-04-16 11:01:19 +0200

    Added email verification

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,17 @@ Route::get('/', 'HomeController')->name('home');
 
 Route::get('search', 'SearchController')->name('search');
 
-Route::get('apps', 'AppController@index')->middleware('auth')->name('app.index');
-Route::get('apps/create', 'AppController@create')->middleware('auth')->name('app.create');
-Route::get('apps/{name}/edit', 'AppController@edit')->middleware('auth')->name('app.edit');
+Route::get('apps', 'AppController@index')->middleware('verified')->name('app.index');
+Route::get('apps/create', 'AppController@create')->middleware('verified')->name('app.create');
+Route::get('apps/{name}/edit', 'AppController@edit')->middleware('verified')->name('app.edit');
 Route::post('apps', 'AppController@store')->name('app.store');
 
-Route::put('apps/{name}', 'AppController@update')->middleware('auth')->name('app.update');
-Route::delete('apps/{name}', 'AppController@destroy')->middleware('auth')->name('app.destroy');
+Route::put('apps/{name}', 'AppController@update')->middleware('verified')->name('app.update');
+Route::delete('apps/{name}', 'AppController@destroy')->middleware('verified')->name('app.destroy');
 
-Route::post('apps/{product}/approve', 'DashboardController@update')->middleware('auth')->name('app.product.approve');
-Route::post('apps/{product}/revoke', 'DashboardController@update')->middleware('auth')->name('app.product.revoke');
-Route::delete('apps/{id}/complete', 'DashboardController@destroy')->middleware('auth')->name('app.products.complete');
+Route::post('apps/{product}/approve', 'DashboardController@update')->middleware('verified')->name('app.product.approve');
+Route::post('apps/{product}/revoke', 'DashboardController@update')->middleware('verified')->name('app.product.revoke');
+Route::delete('apps/{id}/complete', 'DashboardController@destroy')->middleware('verified')->name('app.products.complete');
 
 Route::get('products', 'ProductController@index');
 Route::get('products/{product:slug}', 'ProductController@show')->name('product.show');
@@ -42,12 +42,12 @@ Route::get('faq', 'FaqController@index')->name('faq.index');
 Route::get('contact', 'ContactController@index')->name('contact.index');
 Route::post('contact/sendMail', 'ContactController@sendMail')->name('contact.sendEmail');
 
-Route::get('profile', 'UserController@show')->middleware('auth')->name('user.profile');
-Route::put('profile/{user}/update', 'UserController@update')->middleware('auth')->name('user.profile.update');
-Route::post('profile/update/picture', 'UserController@updateProfilePicture')->middleware('auth')->name('user.profile.update.picture');
+Route::get('profile', 'UserController@show')->middleware('verified')->name('user.profile');
+Route::put('profile/{user}/update', 'UserController@update')->middleware('verified')->name('user.profile.update');
+Route::post('profile/update/picture', 'UserController@updateProfilePicture')->middleware('verified')->name('user.profile.update.picture');
 
-Route::get('dashboard', 'DashboardController@index')->middleware('auth')->name('dashboard');
+Route::get('dashboard', 'DashboardController@index')->middleware('verified')->name('dashboard');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('{content:slug}', 'ContentController@show')->name('page.show');
