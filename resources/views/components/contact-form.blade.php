126bce223e8eb1ff6cd5b9c5d3f4d850ebd848c7
Jethro May <jethro@plusnarrative.com>
2020-04-20 15:04:26 +0200

    Contact page restructure

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -5,14 +5,11 @@
 <div class="contact-form mb-4">
 	<form action="{{ route('contact.send') }}" id="contact-form" method="POST">
 		@csrf
-		<h1 class="mb-4">{{ !empty($title) ? $title : 'Get in touch' }}</h1>
-		@isset($slot)
-		<p class="mb-2">{{$slot}}</p>
-		@endisset
 		<input type="text" name="first_name" placeholder="Enter first name" autocomplete="first_name">
 		<input type="text" name="last_name" placeholder="Enter last name" autocomplete="last_name">
 		<input type="email" name="email" placeholder="Enter email address" autocomplete="email">
 		<textarea name="message" placeholder="Enter message" rows="4"></textarea>
+
 		<button>Send message</button>
 	</form>
 </div>
--- resources/views/templates/contact/index.blade.php
+++ resources/views/templates/contact/index.blade.php
@@ -9,17 +9,44 @@
 @section("content")
     <x-heading heading="Contact us"></x-heading>
 
-	<section class="faqs-section section-padding mt-4 mb-4">
-		<h1>Need more Help?</h1>
-		<p> <span>Want to know about available data, localisation, sandbox, costs and more? So do many others.</span>
-		<span>Check out the answers to some frequently asked questions.</span></p>
-		<a class="button arrow-right after" href="/faq" role="button">View FAQ's</a>
-	</section>
+    <section>
+        <div class="container">
+            <x-action-tab
+                link="https://spectrum.chat/mtn-developer-hub"
+                text="For more help, connect with us on Spectrum"
+                logo="spectrum">
+            </x-action-tab>
 
-    <section class="grey-bg mb-4">
-		<div class="columns">
-			<div class="column contact-details">
-				<h3>Contact Details</h3>
+            <x-action-tab
+                link="https://madapi.statuspage.io/"
+                title="Network status."
+                text="See more on our status page"
+                status="green">
+            </x-action-tab>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="column">
+                <h2>Need more help?</h2>
+
+                <p>
+                    Want to know about available data, localisation, sandbox, costs and more? So do many others. Check out the answers to some frequentlky asked questions.
+                </p>
+
+                <button>
+                    View FAQ's
+                    @svg('arrow-forward')
+                </button>
+            </div>
+            <div class="column">
+                <h2>Get in touch</h2>
+
+                <x-contact-form></x-contact-form>
+            </div>
+            <div class="column">
+                <h2>Contact details</h2>
                 <address>
                     <div class="left">
                         @svg('map-marker', '#FC0')
@@ -34,17 +61,30 @@
                         <a href="tel:+27 11 912 3000">+27 11 912 3000</a>
                     </div>
                 </address>
-			</div>
-			<div class="column">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3581.4426301231297!2d27.92883555117222!3d-26.149710368030952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e950aa4037c8d6b%3A0x5e0ccf872377c77e!2sMTN!5e0!3m2!1sen!2sza!4v1585650125707!5m2!1sen!2sza" width="100%" height="427" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-			</div>
-		</div>
-	</section>
-
-    <section class="contact-form section-padding">
-        <div class="container">
-            <x-contact-form title="Need more help? Get in touch"></x-contact-form>
+            </div>
         </div>
-	</section>
+    </section>
+
+{{--    <section class="grey-bg mb-4">--}}
+{{--		<div class="columns">--}}
+{{--			<div class="column contact-details">--}}
+{{--				<h3>Contact Details</h3>--}}
+{{--                <address>--}}
+{{--                    <div class="left">--}}
+{{--                        @svg('map-marker', '#FC0')--}}
+{{--                        <div>--}}
+{{--                            <span>14th Avenue</span>--}}
+{{--                            <span>Johannesburg</span>--}}
+{{--                            <span>2196, South Africa</span>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="right">--}}
+{{--                        @svg('phone', '#FC0')--}}
+{{--                        <a href="tel:+27 11 912 3000">+27 11 912 3000</a>--}}
+{{--                    </div>--}}
+{{--                </address>--}}
+{{--			</div>--}}
+{{--		</div>--}}
+{{--	</section>--}}
 
 @endsection
