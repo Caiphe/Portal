44e8706797f1be50a07e87feffa832e9b6029180
Wesley Martin <wes@plusnarrative.com>
2020-04-21 09:30:53 +0200

    Updated verify view to have an alert when resend is pressed. Tested on verify email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -11,6 +11,11 @@
 <form method="POST" action="{{ route('verification.resend') }}">
     @csrf
 	<h1>Almost there...</h1>
+    @if (session('resent'))
+    <div class="resent-success" role="alert">
+        A fresh verification link has been sent to your email address.
+    </div>
+    @endif
 	<p>A confirmation email has been sent to your email address. Please click on the link in the email to verify your email address.</p>
 	<p>If you are not receiving the confirmation email, please read out FAQ article <a href="/faq">here.</a></p>
     <button type="submit">Resend verification</button>
