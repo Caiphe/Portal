d94aa2bb2a39bac8764ad43475ef872a8f13e054
Wesley Martin <wes@plusnarrative.com>
2020-04-23 14:17:29 +0200

    Fixing issue when the user isn't logged in

--- app/Product.php
+++ app/Product.php
@@ -7,65 +7,56 @@ use App\KeyFeature;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Product extends Model
-{
-    /**
-     * The attributes that aren't mass assignable.
-     *
-     * @var array
-     */
-    protected $guarded = [];
-    protected $primaryKey = "pid";
-    public $incrementing = false;
+class Product extends Model {
+	/**
+	 * The attributes that aren't mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $guarded = [];
+	protected $primaryKey = "pid";
+	public $incrementing = false;
 
-    public function setNameAttribute($value)
-    {
-        $this->attributes['name'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
-    }
+	public function setNameAttribute($value) {
+		$this->attributes['name'] = $value;
+		$this->attributes['slug'] = Str::slug($value);
+	}
 
-    public function scopeHasSwagger($query)
-    {
-        return $query->whereNotNull('swagger');
-    }
+	public function scopeHasSwagger($query) {
+		return $query->whereNotNull('swagger');
+	}
 
-    public function scopeIsPublic($query)
-    {
-        return $query->hasSwagger()->whereAccess("public");
-    }
+	public function scopeIsPublic($query) {
+		return $query->hasSwagger()->whereAccess("public");
+	}
 
-    public function scopeIsPublicWithInternal($query)
-    {
-        return $query->hasSwagger()->where(function ($query) {
-            $query->where('access', 'public')
-                ->orWhere('access', 'internal');
-        });
-    }
+	public function scopeIsPublicWithInternal($query) {
+		return $query->hasSwagger()->where(function ($query) {
+			$query->where('access', 'public')
+				->orWhere('access', 'internal');
+		});
+	}
 
-    public function scopeBasedOnUser($query, $user)
-    {
-        if ($user->hasPermissionTo('view_internal_products')) {
-            return $query->isPublicWithInternal();
-        }
-        
-        return $query->isPublic();
-    }
+	public function scopeBasedOnUser($query, $user) {
+		if ($user && $user->hasPermissionTo('view_internal_products')) {
+			return $query->isPublicWithInternal();
+		}
 
-    public function scopeGetEnvironment($query, $environment)
-    {
-        return $query
-            ->isPublic()
-            ->hasSwagger()
-            ->whereRaw("find_in_set('$environment',environments)");
-    }
+		return $query->isPublic();
+	}
 
-    public function content()
-    {
-        return $this->belongsToMany(Content::class, "content_product", "product_pid");
-    }
+	public function scopeGetEnvironment($query, $environment) {
+		return $query
+			->isPublic()
+			->hasSwagger()
+			->whereRaw("find_in_set('$environment',environments)");
+	}
 
-    public function keyFeatures()
-    {
-        return $this->belongsToMany(KeyFeature::class, "key_feature_product", "product_pid", "key_feature_id");
-    }
+	public function content() {
+		return $this->belongsToMany(Content::class, "content_product", "product_pid");
+	}
+
+	public function keyFeatures() {
+		return $this->belongsToMany(KeyFeature::class, "key_feature_product", "product_pid", "key_feature_id");
+	}
 }
