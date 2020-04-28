80de3acf819916fc328a5615ae35d9c8568dc24f
Jethro May <jethro@plusnarrative.com>
2020-04-28 08:09:53 +0200

    Indentation

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Category;
 
-class FaqController extends Controller {
+class FaqController extends Controller
+{
 	public function index() {
 		$categories = Category::with('faqs:id,category_id,question,answer')->whereHas('faqs')->get();
 		$categoryLookup = array_map('strtolower', $categories->pluck('title', 'id')->toArray());
 		$faqs = call_user_func_array('array_merge', array_column($categories->toArray(), 'faqs'));
-		return view('templates.faq.index', [
-			'categories' => $categories,
-			'categoryLookup' => $categoryLookup,
-			'faqs' => $faqs,
-		]
+
+            return view('templates.faq.index', [
+                'categories' => $categories,
+                'categoryLookup' => $categoryLookup,
+                'faqs' => $faqs,
+            ]
 		);
 	}
 }
