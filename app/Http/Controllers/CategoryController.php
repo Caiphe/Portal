41b80f8704de989f8504a0d18f2564a438312e39
Wesley Martin <wes@plusnarrative.com>
2020-08-31 15:55:58 +0200

    Adding db connection for content

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        $category->load(['products', 'bundles']);
+        $category->load(['products', 'bundles', 'content']);
 
         return view(
             'templates.category.show',
@@ -24,6 +24,25 @@ class CategoryController extends Controller
                 'theme' => $category->theme,
                 'category' => $category->title,
                 'slug' => $category->slug,
+                'content' => $category->content->groupBy('type'),
+                'products' => $category->products->map(function ($product) {
+                    return [
+                        'name' => $product['name'],
+                        'group' => $product['group'],
+                        'description' => $product['description'] ?: 'View the product',
+                        'locations' => explode(',', $product['locations'] ?? 'all'),
+                        'href' => route('product.show', $product['slug']),
+                    ];
+                })->toArray(),
+                'bundles' => $category->bundles->map(function ($bundle) {
+                    return [
+                        'name' => $bundle['name'],
+                        'group' => $bundle['group'],
+                        'description' => $bundle['description'] ?: 'View the bundle',
+                        'locations' => explode(',', $bundle['locations'] ?? 'all'),
+                        'href' => route('bundle.show', $bundle['slug']),
+                    ];
+                })->toArray(),
             ]
         );
     }
