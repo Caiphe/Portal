b345954efee43ffe0f1d6c375e02b84a2b50465b
Wesley Martin <wes@plusnarrative.com>
2020-09-08 17:35:08 +0200

    Adding throttle on contact page

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::get('faq', 'FaqController@index')->name('faq.index');
 Route::get('faq/{faq:slug}', 'FaqController@show')->name('faq.show');
 
 Route::get('contact', 'ContactController@index')->name('contact.index');
-Route::post('contact', 'ContactController@send')->name('contact.send');
+Route::post('contact', 'ContactController@send')->middleware('throttle:1,1')->name('contact.send');
 
 Auth::routes(['verify' => true]);
 
