74839777e8d1dc0ccee6b5c0d1392f005f66a3fc
Jethro May <jethro@plusnarrative.com>
2020-09-08 13:32:24 +0200

    Various responsive adjustments for mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,12 @@
 <header id="header">
     <nav class="header-inner container">
         <a class="site-name" href="/"><img src="/images/mtn-logo.svg" alt="MTN logo"> Developer Portal</a>
-        <ul class="main-menu" role="navigation" aria-label="Main">
+        <button type="button" class="menu-button">
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        <ul class="main-menu hidden" role="navigation" aria-label="Main">
             <li class="has-children">
                 <a href="/products">Products @svg("chevron-down")</a>
                 <div class="product-nav shadow">
@@ -28,13 +33,13 @@
             <li><a href="/dashboard">Dashboard</a></li>
             @endcan
         </ul>
-        <form action="{{route('search')}}">
+        <form class="hidden" action="{{route('search')}}">
             <input type="search" name="q" id="search" class="thin see-through" placeholder="Search">
             @svg('search')
         </form>
         @if(\Auth::check())
-            <a href="/apps/create" class="button dark" role="button">Build app</a>
-            <div id="profile-menu">
+            <a href="/apps/create" class="button dark hidden" role="button">Build app</a>
+            <div id="profile-menu hidden">
                 <div id="profile-menu-picture" style="background-image: url({{\Auth::user()->profile_picture}})"></div>
                 <ul class="profile-menu-options shadow">
                     <li><a href="/profile">Profile</a></li>
@@ -43,8 +48,8 @@
                 </ul>
             </div>
         @else
-            <a href="{{route('login')}}" class="button dark outline mr-1" role="button">Login</a>
-            <a href="{{route('register')}}" class="button dark" role="button">Register</a>
+            <a href="{{route('login')}}" class="button dark outline mr-1 hidden" role="button">Login</a>
+            <a href="{{route('register')}}" class="button dark hidden" role="button">Register</a>
         @endif
     </nav>
 </header>
--- resources/views/templates/home.blade.php
+++ resources/views/templates/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{"MTN":"Developer Portal"}</title>
+    <title>MTN Developer Portal</title>
     <link rel="icon" href="/images/favicon.svg" type="image/svg+xml">
     <link rel="icon" href="/images/favicon.png" type="image/png">
     <link rel="stylesheet" href="{{ mix('/css/styles.css') }}">
