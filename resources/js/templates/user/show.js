3aeeabd3a0d363030de7c0bcfb696e432df0e50e
Wesley Martin <wes@plusnarrative.com>
2021-06-17 10:33:30 +0200

    Added better password validation on profile page

--- resources/js/templates/user/show.js
+++ resources/js/templates/user/show.js
@@ -61,5 +61,73 @@ function uploadProfilePicture(file) {
 
 function togglePasswordVisibility(that) {
     that.parentNode.classList.toggle('password-visible');
-    that.previousElementSibling.setAttribute('type',that.parentNode.classList.contains('password-visible') ? "text" : "password");
+    that.previousElementSibling.setAttribute('type', that.parentNode.classList.contains('password-visible') ? "text" : "password");
 }
+
+(function() {
+    var passwordScore = 0;
+
+    document.getElementById('password').addEventListener('input', checkPassword);
+    document.getElementById('profile-form').addEventListener('submit', validateSubmit);
+
+    function checkPassword() {
+        var value = this.value;
+        var passwordErrors = [
+            'Password must be 12 characters or more.',
+            'Password must have at least one special character',
+            'Password must have at least one number',
+            'Password must have at least one uppercase character',
+            'Password must have at least one lowercase character',
+        ];
+        var failed = [];
+
+        if (value.length < 12) failed.push(passwordErrors[0]);
+        if (!/[!@#\$%\^\&\*\(\)_\+=\-\{\}|"':<>\?~`\.]/.test(value)) failed.push(passwordErrors[1]);
+        if (!/[0-9]/.test(value)) failed.push(passwordErrors[2]);
+        if (!/[A-Z]/.test(value)) failed.push(passwordErrors[3]);
+        if (!/[a-z]/.test(value)) failed.push(passwordErrors[4]);
+
+        passwordScore = passwordErrors.length - failed.length;
+        document.getElementById('password-still-needs').innerHTML = failed.join('<br>');
+        document.getElementById('password-strength').className = 'password-score-' + passwordScore;
+    }
+
+    function validateSubmit(ev) {
+        var firstName = this.elements['first_name'].value;
+        var lastName = this.elements['last_name'].value;
+        var email = this.elements['email'].value;
+        var password = this.elements['password'].value;
+        var confirmPassword = this.elements['password_confirmation'].value;
+        var errors = [];
+
+        if (firstName === '') {
+            errors.push('Please add a valid first name');
+        }
+
+        if (lastName === '') {
+            errors.push('Please add a valid last name');
+        }
+
+        if (email === '' || !/\w@\w/.test(email)) {
+            errors.push('Please add a valid email');
+        }
+
+        if (password !== '' && confirmPassword === '') {
+            errors.push('Please confirm your password');
+        } else if (confirmPassword !== password) {
+            errors.push('Your passwords do not match');
+        }
+
+        if (password !== '' && passwordScore !== 5) {
+            errors.push('Your password is not strong enough');
+        }
+
+        if (errors.length > 0) {
+            addAlert('error', errors);
+            ev.preventDefault();
+            return;
+        }
+
+        addLoading('Updating your profile.');
+    }
+}());
