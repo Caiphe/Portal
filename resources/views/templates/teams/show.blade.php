c7d86ec07117a3779c88593409785d1772dd5284
Wesley Martin <wes@plusnarrative.com>
2021-11-15 12:10:28 +0200

    Show message if a transfer owner ship request is sent on current owners screen

--- resources/views/templates/teams/show.blade.php
+++ resources/views/templates/teams/show.blade.php
@@ -344,6 +344,17 @@ My team
             </div>
         </div>
     </div>
+    @elseif ($userTeamOwnershipRequest)
+    <div class="transfer-ownership-container" id="transfer-ownership">
+        <div class="transfer-owner-ship-heading">
+            <h2>Transfer ownership</h2>
+        </div>
+
+        <div class="trasfer-container">
+            <h4>Transfer requests sent</h4>
+            <div class="site-text">A transfer request must be accepted by the team member before the team ownership is confirmed.</div>
+        </div>
+    </div>
     @endif
 
     <div class="column" id="app-index">
