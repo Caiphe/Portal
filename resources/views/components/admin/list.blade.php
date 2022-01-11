26c5554d33fbe94ef537fbdfbb21d3aa284c874a
Wesley Martin <wes@plusnarrative.com>
2022-01-11 08:17:36 +0200

    Changed how long copy is handled in table cells

--- resources/views/components/admin/list.blade.php
+++ resources/views/components/admin/list.blade.php
@@ -1,37 +1,35 @@
-<div class="table-data-list">
-    <table>
-        <thead>
-            <tr>
-                @foreach($fields as $specifiedName => $field)
-                <th align="left"><a href="?sort={{ strtok($field, '|') }}&order={{ $order ?? 'desc' }}">{{ is_string($specifiedName) ? $specifiedName : preg_replace('/[_\.]/', ' ', $field) }} @svg('chevron-sorter')</a></th>
-                @endforeach
-                <th align="left" class="action-row">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($collection as $model)
-            <tr>
-                @foreach($fields as $field)
-                <td align="left">
-                    @listFunc($field, $model)
-                </td>
-                @endforeach
-                <td class="action-row">
-                    @if(Route::has("{$modelName}.show"))
-                    <a href="{{ route("{$modelName}.show", $model->slug) }}" target="_blank" rel="noreferrer">@svg('eye') View</a>
-                    @endif
-                    <a href="{{ route("admin.{$modelName}.edit", $model->slug) }}">@svg('pencil') Edit</a>
-                    @if(Route::has("admin.{$modelName}.delete"))
-                    <form class="delete-form ajaxify" action="{{ route("admin.{$modelName}.delete", $model->slug) }}" method="POST" data-func="removeRow({{ $model->slug }})" data-confirm="Are you sure you want to delete this?">
-                        @method('DELETE')
-                        @csrf
-                        <button class="sl-button">@svg('trash') Delete</button>
-                    </form>
-                    @endif
-                </td>
-            </tr>
+<table>
+    <thead>
+        <tr>
+            @foreach($fields as $specifiedName => $field)
+            <th align="left"><a href="?sort={{ strtok($field, '|') }}&order={{ $order ?? 'desc' }}">{{ is_string($specifiedName) ? $specifiedName : preg_replace('/[_\.]/', ' ', $field) }} @svg('chevron-sorter')</a></th>
             @endforeach
-        </tbody>
-    </table>
-</div>
+            <th align="left" class="action-row">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($collection as $model)
+        <tr>
+            @foreach($fields as $field)
+            <td align="left" class="elipsise">
+                @listFunc($field, $model)
+            </td>
+            @endforeach
+            <td class="action-row">
+                @if(Route::has("{$modelName}.show"))
+                <a href="{{ route("{$modelName}.show", $model->slug) }}" target="_blank" rel="noreferrer">@svg('eye') View</a>
+                @endif
+                <a href="{{ route("admin.{$modelName}.edit", $model->slug) }}">@svg('pencil') Edit</a>
+                @if(Route::has("admin.{$modelName}.delete"))
+                <form class="delete-form ajaxify" action="{{ route("admin.{$modelName}.delete", $model->slug) }}" method="POST" data-func="removeRow({{ $model->slug }})" data-confirm="Are you sure you want to delete this?">
+                    @method('DELETE')
+                    @csrf
+                    <button class="sl-button">@svg('trash') Delete</button>
+                </form>
+                @endif
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 {{ $collection->withQueryString()->links() }}
