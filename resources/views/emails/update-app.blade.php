b16f445a55778bc50546fa5c6aad7b9d5d69dcb7
Wesley Martin <wes@plusnarrative.com>
2022-02-07 13:05:03 +0200

    Added fallback if app was created by a team and not a developer

--- resources/views/emails/update-app.blade.php
+++ resources/views/emails/update-app.blade.php
@@ -6,7 +6,7 @@
 |:------------------|:---|:----------|
 | **App** | | {{ $app->display_name }} |
 | **Country** | | {{ $app->country->name }} |
-| **Developer** | | {{ $app->developer->full_name }} |
+| **Developer** | | {{ $app->developer->full_name ?? $app->team->name }} |
 | **Developer Email** | | <a href="mailto:{{ $app->developer->email }}">{{ $app->developer->email }}</a> |
 | **Products** | | {{ $app->products->implode('display_name', ', ') }} |
 
