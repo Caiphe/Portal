de5e0eb4a678181248a52f636cd158209dc2c711
marc@plusnarrative.com <marc@plusnarrative.com>
2022-04-25 09:46:04 +0200

    js functions optimized

--- resources/js/templates/admin/dashboard/index.js
+++ resources/js/templates/admin/dashboard/index.js
@@ -86,105 +86,105 @@
         var customAttributeDialog = document.getElementById('custom-attributes-' + id);
         if (!customAttributeDialog) return;
         customAttributeDialog.classList.add('show');
-
         var addAttributeBtn = customAttributeDialog.querySelector('.add-attribute');
-        var attributeName = customAttributeDialog.querySelector('.attribute-name');
-        var attributeValue = customAttributeDialog.querySelector('.attribute-value');
-        var attributeErrorMessage = customAttributeDialog.querySelector('.attribute-error');
         var attributesList = customAttributeDialog.querySelector('.custom-attributes-list');
 
-        addAttributeBtn.addEventListener('click', addNewAttribute);
-
+        addAttributeBtn.addEventListener('click', addNewAttribute.bind(customAttributeDialog, attributesList));
         customAttributeDialog.addEventListener('dialog-closed', submitNewAttribute.bind(attributesList, id));
+    }
 
-        function addNewAttribute(){
-            if(attributeName.value === "" || attributeValue.value === ''){
-                attributeErrorMessage.classList.add('show');
-                return;
-            }
-            attributesList.classList.add('active');
-            attributeErrorMessage.classList.remove('show');
-            var customAttributeBlock = document.getElementById('custom-attribute').innerHTML;
-            customAttributeBlock = document.createRange().createContextualFragment(customAttributeBlock);
-            customAttributeBlock.querySelector('.name').value = attributeName.value;
-            customAttributeBlock.querySelector('.value').value = attributeValue.value;
-            attributesList.appendChild(customAttributeBlock);
-            customAttributeDialog.querySelector('.attributes-heading').classList.add('show');
-            attributeName.value = '';
-            attributeValue.value = '';
+    function submitNewAttribute(id){
+        var elements = this.elements;
+        var attrNames = elements['attribute[name][]'];
+        var attrValues = elements['attribute[value][]'];
+
+        var app = {
+            attribute: [],
+            _method: 'PUT',
+            _token: elements['_token'].value,
+        };
+
+        if(attrNames && attrNames.length === undefined) {
+            attrNames = [attrNames];
+            attrValues = [attrValues];
         }
 
-        function submitNewAttribute(id){
-            var elements = this.elements;
-            var attrNames = elements['attribute[name][]'];
-            var attrValues = elements['attribute[value][]'];
-
-
-            var app = {
-                attribute: [],
-                _method: 'PUT',
-                _token: elements['_token'].value,
-            };
-
-            if(attrNames && attrNames.length === undefined) {
-                attrNames = [attrNames];
-                attrValues = [attrValues];
+        if(attrNames){
+            for(var i = 0; i < attrNames.length; i++) {
+                app.attribute.push({
+                    'name': attrNames[i].value,
+                    'value': attrValues[i].value
+                });
             }
-
-            if(attrNames){
-                for(var i = 0; i < attrNames.length; i++) {
-                    app.attribute.push({
-                        'name': attrNames[i].value,
-                        'value': attrValues[i].value
-                    });
-                }
-            }
-
-            var xhr = new XMLHttpRequest();
-
-            event.preventDefault();
-            addLoading('Updating...');
-
-            xhr.open('POST', this.action, true);
-            xhr.setRequestHeader('X-CSRF-TOKEN', elements['_token'].value);
-            xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-            xhr.send(JSON.stringify(app));
-
-            xhr.onload = function() {
-                removeLoading();
-                var result = xhr.responseText ? JSON.parse(xhr.responseText) : null;
-                var attrHtml = '';
-
-                if (xhr.status === 200) {
-
-                    for (key in result['attributes']) {
-                        attrHtml += `
-                        <div class="attribute-display">
-                            <span class="attr-name bold">${key} : </span>
-                            <span class="attr-value">${result['attributes'][key]}</span>
-                        </div>
-                        `;
-                    }
-
-                    document.querySelector('#wrapper-'+id+' .list-custom-attributes').innerHTML = attrHtml;
-
-                    addAlert('success', ['Custom attributes added successfully',]);
-                } else {
-
-                    if(result.errors) {
-                        result.message = [];
-                        for(var error in result.errors){
-                            result.message.push(result.errors[error]);
-                        }
-                    }
-
-                    addAlert('error', result.message || 'Sorry there was a problem updating your app. Please try again.');
-                    button.removeAttribute('disabled');
-                }
-            };
         }
+
+        var xhr = new XMLHttpRequest();
+
+        event.preventDefault();
+        addLoading('Updating...');
+
+        xhr.open('POST', this.action, true);
+        xhr.setRequestHeader('X-CSRF-TOKEN', elements['_token'].value);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+        xhr.send(JSON.stringify(app));
+
+        xhr.onload = function() {
+            removeLoading();
+            var result = xhr.responseText ? JSON.parse(xhr.responseText) : null;
+            var attrHtml = '';
+
+            if (xhr.status === 200) {
+
+                for (key in result['attributes']) {
+                    attrHtml += `
+                    <div class="attribute-display">
+                        <span class="attr-name bold">${key} : </span>
+                        <span class="attr-value">${result['attributes'][key]}</span>
+                    </div>
+                    `;
+                }
+
+                document.querySelector('#wrapper-'+id+' .list-custom-attributes').innerHTML = attrHtml;
+
+                addAlert('success', ['Custom attributes added successfully',]);
+            } else {
+
+                if(result.errors) {
+                    result.message = [];
+                    for(var error in result.errors){
+                        result.message.push(result.errors[error]);
+                    }
+                }
+
+                addAlert('error', result.message || 'Sorry there was a problem updating your app. Please try again.');
+                button.removeAttribute('disabled');
+            }
+        };
+    }
+
+    function addNewAttribute(attributesList){
+        var attributeName = this.querySelector('.attribute-name');
+        var attributeValue = this.querySelector('.attribute-value');
+        var attributeErrorMessage = this.querySelector('.attribute-error');
+
+
+        if(attributeName.value === "" || attributeValue.value === ''){
+            attributeErrorMessage.classList.add('show');
+            return;
+        }
+
+        attributesList.classList.add('active');
+        attributeErrorMessage.classList.remove('show');
+        var customAttributeBlock = document.getElementById('custom-attribute').innerHTML;
+        customAttributeBlock = document.createRange().createContextualFragment(customAttributeBlock);
+        customAttributeBlock.querySelector('.name').value = attributeName.value;
+        customAttributeBlock.querySelector('.value').value = attributeValue.value;
+        attributesList.appendChild(customAttributeBlock);
+        this.querySelector('.attributes-heading').classList.add('show');
+        attributeName.value = '';
+        attributeValue.value = '';
     }
 
     function showProductNoteDialog() {
