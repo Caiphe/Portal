9c2dce8972f8f8b3190a21117957fb5c21d91241
marc@plusnarrative.com <marc@plusnarrative.com>
2022-05-13 09:09:51 +0200

    log fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -46,6 +46,7 @@ class DashboardController extends Controller
             return view('templates.admin.dashboard.index', [
                 'apps' => App::where('country_code', 'none')->paginate($numberPerPage),
                 'countries' => Country::all(),
+                'previous' => null,
                 'selectedCountry' => $request->get('countries', ''),
                 'appStatus' => $request->get('app-status', 'pending'),
                 'productStatus' => $request->get('product-status', 'pending')
