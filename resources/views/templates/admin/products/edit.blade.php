b65f4ee54c962fca93aae839798ee861a55917f7
marc@plusnarrative.com <marc@plusnarrative.com>
2022-05-18 21:28:57 +0200

    product update log layout

--- resources/views/templates/admin/products/edit.blade.php
+++ resources/views/templates/admin/products/edit.blade.php
@@ -30,35 +30,106 @@
     @method('PUT')
     @csrf
 
-    <div class="editor-field one-third">
-        <h2>Product details</h2>
+    <div class="one-third">
 
-        <label class="editor-field-label">
-            <h3>Display name</h3>
-            <input type="text" name="display_name" value="{{ $product->display_name }}" maxlength="140">
-        </label>
+        <div class="editor-field">
+            <h2>Product details</h2>
 
-        <label class="editor-field-label">
-            <h3>Category</h3>
-            <select name="category_cid" id="category_cid" class="mb-1" autocomplete="off">
-                <option value="" selected disabled="">Select category</option>
-                @foreach($categories as $cid => $category)
-                <option value="{{ $cid }}" @if($cid === $product->category_cid) selected @endif>{{ $category }}</option>
-                @endforeach
-            </select>
-        </label>
+            <label class="editor-field-label">
+                <h3>Display name</h3>
+                <input type="text" name="display_name" value="{{ $product->display_name }}" maxlength="140">
+            </label>
 
-        <label class="editor-field-label">
-            <h3>Group</h3>
-            <input type="text" name="group" value="{{ $product->group ?? 'MTN' }}">
-        </label>
+            <label class="editor-field-label">
+                <h3>Category</h3>
+                <select name="category_cid" id="category_cid" class="mb-1" autocomplete="off">
+                    <option value="" selected disabled="">Select category</option>
+                    @foreach($categories as $cid => $category)
+                    <option value="{{ $cid }}" @if($cid === $product->category_cid) selected @endif>{{ $category }}</option>
+                    @endforeach
+                </select>
+            </label>
 
-        <label class="editor-field-label">
-            <h3>Locations</h3>
-            <x-multiselect id="locations" name="locations" label="Select location" :options="$countries->pluck('name', 'code')->toArray()" :selected="$product->locations === 'all' ? [] : $product->countries()->pluck('code')->toArray()"/>
-        </label>
+            <label class="editor-field-label">
+                <h3>Group</h3>
+                <input type="text" name="group" value="{{ $product->group ?? 'MTN' }}">
+            </label>
 
-        <button class="button outline blue save-button">Apply changes</button>
+            <label class="editor-field-label">
+                <h3>Locations</h3>
+                <x-multiselect id="locations" name="locations" label="Select location" :options="$countries->pluck('name', 'code')->toArray()" :selected="$product->locations === 'all' ? [] : $product->countries()->pluck('code')->toArray()"/>
+            </label>
+
+            <button class="button outline blue save-button">Apply changes</button>
+        </div>
+
+        <div class="editor-field">
+            <h2>Product Update Log</h2>
+
+            <div class="product-logs-container">
+
+                <div class="each-log">
+                   @svg('calendar')
+                    <div class="date-block">
+                        <p class="date">9 April 2022</p>
+                        <span class="time">18:30</span>
+                    </div>
+                    <div class="profile-picture"></div>
+
+                    <div class="log-description">Custom attribute added by</div>
+                    <div class="logged-by">Hitesh Jinabhai</div>
+                </div>
+
+                <div class="each-log">
+                    @svg('calendar')
+                    <div class="date-block">
+                        <p class="date">9 April 2022</p>
+                        <span class="time">18:30</span>
+                    </div>
+                    <div class="profile-picture"></div>
+
+                    <div class="log-description">Custom attribute added by</div>
+                    <div class="logged-by">Hitesh Jinabhai</div>
+                </div>
+
+                <div class="each-log">
+                    @svg('calendar')
+                    <div class="date-block">
+                        <p class="date">9 April 2022</p>
+                        <span class="time">18:30</span>
+                    </div>
+                    <div class="profile-picture"></div>
+
+                    <div class="log-description">Custom attribute added by</div>
+                    <div class="logged-by">Hitesh Jinabhai</div>
+                </div>
+
+                <div class="each-log">
+                    @svg('calendar')
+                    <div class="date-block">
+                        <p class="date">9 April 2022</p>
+                        <span class="time">18:30</span>
+                    </div>
+                    <div class="profile-picture"></div>
+
+                    <div class="log-description">Custom attribute added by</div>
+                    <div class="logged-by">Hitesh Jinabhai</div>
+                </div>
+
+                <div class="each-log">
+                    @svg('calendar')
+                    <div class="date-block">
+                        <p class="date">9 April 2022</p>
+                        <span class="time">18:30</span>
+                    </div>
+                    <div class="profile-picture"></div>
+
+                    <div class="log-description">Custom attribute added by</div>
+                    <div class="logged-by">Hitesh Jinabhai</div>
+                </div>
+
+            </div>
+        </div>
     </div>
 
     <div id="custom-tabs" class="editor-field two-thirds">
