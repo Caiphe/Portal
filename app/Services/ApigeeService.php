1a2492e2bd976f3fbe93c250033a002dcc7a727d
Marc Caiphe Ilunga <marc@plusnarrative.com>
2022-09-01 14:49:43 +0200

    product attribute format studly

--- app/Services/ApigeeService.php
+++ app/Services/ApigeeService.php
@@ -371,6 +371,25 @@ class ApigeeService
 
         return $a;
     }
+    public static function getProductAttributes(array $attributes)
+    {
+        $a = [];
+
+        foreach ($attributes as $attribute) {
+            $key = Str::studly($attribute['name']);
+            $value = trim($attribute['value']);
+
+            if (!isset($value)) {
+                $attribute['value'] = '';
+	    
+	   }
+            $value = $key === 'Group' ? Str::studly($value) : $value;
+      
+            $a[$key] = $value;
+        }
+
+        return $a;
+    }
 
     public static function getApigeeAppAttributes(App $app)
     {
