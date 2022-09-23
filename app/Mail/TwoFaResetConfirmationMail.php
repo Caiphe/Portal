46116b94ab3e45cd2491a8628e896194d93a83b1
Marc Caiphe Ilunga <marc@plusnarrative.com>
2022-09-23 11:53:57 +0200

    removed unused constructor function on the mail

--- app/Mail/TwoFaResetConfirmationMail.php
+++ app/Mail/TwoFaResetConfirmationMail.php
@@ -13,16 +13,6 @@ class TwoFaResetConfirmationMail extends Mailable
     use Queueable, SerializesModels;
 
     /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-
-    }
-
-    /**
      * Build the message.
      *
      * @return $this
