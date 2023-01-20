e344836d29b54344ba5045a2e729ed7facb204ad
Marc Caiphe Ilunga <marc@plusnarrative.com>
2023-01-20 14:11:07 +0200

    password reset patch

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'sent' => 'Thank you, If this email address exists, a reset link has been sent.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
