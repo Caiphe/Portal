5202109626d91136e5f0ae42e87763fcc4155697
Marc Caiphe Ilunga <marc@plusnarrative.com>
2023-05-15 16:10:03 +0200

    opco admin role request sent and approval by Opco admin

--- app/Http/Controllers/OpcoRoleRequestController.php
+++ app/Http/Controllers/OpcoRoleRequestController.php
@@ -15,18 +15,26 @@ class OpcoRoleRequestController extends Controller
         $user = auth()->user();
         $data = $request->validated();
         $countries = $data['countries'];
+
+        abort_if(count($countries) > 1, 424, "Only one country allowed per request");
+
         $data['countries'] = implode(',', $countries);
         $data['user_id'] = $user->id;
         $requestCountryCodes = explode(',', $data['countries']);
-
+		$usersCountries = $user->countries;
+        
         OpcoRoleRequest::create($data);
         $countries = Country::whereIn('code', $requestCountryCodes)->pluck('name')->toArray();
         $adminUsers = User::whereHas('roles', fn ($q) => $q->where('name', 'Admin'))->pluck('email')->toArray();
 
-        foreach($adminUsers as $admin){
-            Mail::to($admin)->send(new OpcoAdminRoleRequest($user, $countries));
+        if($usersCountries){
+			$opcoEmails = $usersCountries->load('opcoUser')->pluck('opcoUser')->flatten()->pluck('email')->unique()->values();
+
+            Mail::bcc($opcoEmails)->send(new OpcoAdminRoleRequest($user, $countries));
+			return response()->json(['success' => true, 'code' => 200], 200);  
         }
-        
+
+        Mail::bcc($adminUsers)->send(new OpcoAdminRoleRequest($user, $countries));
         return response()->json(['success' => true, 'code' => 200], 200);
     }
 }
