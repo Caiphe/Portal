c3786d9c234943478e5cef85ee1adaddcb492e40
Marc Caiphe Ilunga <marc@plusnarrative.com>
2023-07-10 16:09:27 +0200

    product location query updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
 		}
 		$inlineUrl = TwofaService::getInlineUrl($key, $user->email);
 
-		$locations = Product::isPublic()
+		$productLocations = Product::isPublic()
 			->WhereNotNull('locations')
 			->Where('locations', '!=', 'all')
 			->select('locations')
@@ -51,7 +51,7 @@ class UserController extends Controller
 			'user' => $user,
 			'userLocations' => $user->countries->pluck('code')->toArray(),
 			'responsibleCountries' => $user->responsibleCountries->pluck('code')->toArray(),
-			'locations' => array_unique(explode(',', $locations)),
+			'locations' => array_unique(explode(',', $productLocations)),
 			'key' => $key,
 			'inlineUrl' => $inlineUrl,
 			'teamInvite' => $teamInvite,
