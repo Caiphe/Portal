01f2fb687caeb14e7cf64d0ef462ce797596aeb6
Tony Moakamela <thabo.tony@gmail.com>
2024-08-27 14:59:23 +0200

    fix: APIGEE team update

--- resources/js/templates/admin/teams/create_form.js
+++ resources/js/templates/admin/teams/create_form.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function removeSpecialCharacters(){
         var specialChrs = /[`~!)@#$%(^&*|+=?;:±§'",.<>\{\}\[\]\\\/]/gi;
         this.value = this.value.replace(/  +/g, ' ');
-    
+
         if(specialChrs.test(this.value)){
             this.value = this.value.replace(specialChrs, '');
             addAlert('warning', 'Team name cannot contain special characters.');
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function validatePhoneNumber(){
         var specialChrs = /[a-z`~!)@#$%(^&*|=?;:±§'",.<>\{\}\[\]\\\/]/gi;
         var containsNonDigits = specialChrs.test(this.value);
-    
+
         if(containsNonDigits){
             addAlert('warning', 'Character not allowed');
         }
